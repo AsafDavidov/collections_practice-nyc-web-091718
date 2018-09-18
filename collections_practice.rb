f7bdfc3559bc3f37a86800113231888f9aa1ee8b
Asaf Davidov <adavidov@conncoll.edu>
2018-09-18 02:15:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,8 +34,9 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  array.select do {|word| word.start_with?(a)}
+  array.select do {|word| word.start_with?("a")}
 end
+
 def sum_array(array)
   sum = 0
   array.each do |number|
