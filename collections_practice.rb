cd15ea1fb0d690da6c2fca7b45ec201e0cda89bb
Asaf Davidov <adavidov@conncoll.edu>
2018-09-18 02:01:08 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,6 @@ def sort_array_desc(unsorted_array)
   unsorted_array.sort {|a,b| b<=>a}
 end
 
-def sort_array_asc(unsorted_array)
+def sort_array_char_count(unsorted_array)
   unsorted_array.sort
 end
